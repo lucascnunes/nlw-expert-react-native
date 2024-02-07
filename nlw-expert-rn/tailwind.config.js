96456faab7c9a32762710e14010f24b3fd29213a
Lucas Nunes <2693803+lucascnunes@users.noreply.github.com>
2024-02-07 17:33:32 -0300

    config: configuring the application

--- nlw-expert-rn/tailwind.config.js
+++ nlw-expert-rn/tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      '.src/app/**/*.{ts,tsx}',
-      '.src/components/**/*.{ts,tsx}'
+      './src/app/**/*.{ts,tsx}',
+      './src/components/**/*.{ts,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        heading: "Inter_600SemiBold",
+        subtitle: "Inter_500Medium",
+        body: "Inter_400Regular",
+        bold: "Inter_700Bold",
+      }
+    },
   },
   plugins: [],
 }
