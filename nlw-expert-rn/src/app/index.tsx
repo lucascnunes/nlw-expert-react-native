2c905ee469a753d059eb9882875cebbf69083e8d
Lucas Nunes <2693803+lucascnunes@users.noreply.github.com>
2024-02-07 14:35:31 -0300

    adding tailwind

---
+++ nlw-expert-rn/src/app/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+
+export default function Home() {
+    return (
+    <View className="bg-slate-900 flex-1">
+        <Text className="text-white">Hello React Native</Text>
+    </View>
+    )
+}
